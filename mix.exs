68bba65549d3309df7b0338050b4543cd48d7a9d
Aksel Stadler <aksel@stadler.no>
2022-01-03 13:23:27 +0100

    add os mon

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Stadlerno.MixProject do
   def application do
     [
       mod: {Stadlerno.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
